25164792bbb6b942e29918e5d5497591d800445a
Aathi Velusamy <Aathi.Velusamy@admins-MacBook-Pro.local>
2016-07-24 21:08:19 +0400

    Initial commit

---
+++ app/src/main/java/a/test/sample/testa/MainActivity.java
@@ -0,0 +1,13 @@
+package a.test.sample.testa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
